9324fb40b2ea8caa11a67a226362e59fc9088ece
Arjan Meskers <a.meskers@amsoftware.nl>
2023-10-22 15:24:23 +0200

    Set-DataverseRow

---
+++ src/AMSoftware.Dataverse.PowerShell/Commands/SetRowCommand.cs
@@ -0,0 +1,61 @@
+﻿using AMSoftware.Dataverse.PowerShell.ArgumentCompleters;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+using System.Collections;
+using System.Management.Automation;
+
+namespace AMSoftware.Dataverse.PowerShell.Commands
+{
+    [Cmdlet(VerbsCommon.Set, "DataverseRow", DefaultParameterSetName = SetObjectParameterSet)]
+    [OutputType(typeof(EntityReference))]
+    public sealed class SetRowCommand : CmdletBase
+    {
+        private const string SetObjectParameterSet = "SetObject";
+        private const string SetValuesParameterSet = "SetValues";
+
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = SetObjectParameterSet, ValueFromPipeline = true)]
+        [Alias("Row", "Entity")]
+        [ValidateNotNullOrEmpty]
+        public Entity InputObject { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = SetValuesParameterSet)]
+        [ValidateNotNullOrEmpty]
+        [ArgumentCompleter(typeof(TableNameArgumentCompleter))]
+        [Alias("LogicalName")]
+        public string Table { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = SetValuesParameterSet)]
+        [ValidateNotNullOrEmpty]
+        public Guid Id { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = SetValuesParameterSet)]
+        [ValidateNotNullOrEmpty]
+        public Hashtable Values { get; set; }
+
+        protected override void Execute()
+        {
+            Entity newEntity = InputObject;
+            if (ParameterSetName == SetValuesParameterSet) newEntity = BuildEntityFromValues();
+
+            OrganizationRequest request = new UpdateRequest() { 
+                Target = newEntity
+            };
+            var response = Session.Current.Client.ExecuteOrganizationRequest(request, MyInvocation.MyCommand.Name);
+            
+            WriteObject(newEntity.ToEntityReference());
+        }
+
+        private Entity BuildEntityFromValues()
+        {
+            var result = new Entity(Table, Id);
+
+            foreach (var attributeName in Values.Keys)
+            {
+                result.Attributes.Add((string)attributeName, Values[attributeName]);
+            }
+
+            return result;
+        }
+    }
+}
