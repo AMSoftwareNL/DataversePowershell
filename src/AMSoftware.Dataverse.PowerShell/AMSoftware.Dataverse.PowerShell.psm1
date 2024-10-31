fb7ebc9338fadfd1223ebedd8228406fb4c1efea
Arjan Meskers <a.meskers@rubicon.nl>
2024-10-31 16:42:10 +0100

    Update Accelerators

--- src/AMSoftware.Dataverse.PowerShell/AMSoftware.Dataverse.PowerShell.psm1
+++ src/AMSoftware.Dataverse.PowerShell/AMSoftware.Dataverse.PowerShell.psm1
@@ -2,12 +2,10 @@ New-Variable -Name DataverseSession
 New-Variable -Name DataverseClient
 
 [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvmoney', [Microsoft.Xrm.Sdk.Money])
-[psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvchoice', [Microsoft.Xrm.Sdk.OptionSetValue])
+[psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvchoicevalue', [Microsoft.Xrm.Sdk.OptionSetValue])
 [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvrow', [Microsoft.Xrm.Sdk.Entity])
 [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvreference', [Microsoft.Xrm.Sdk.EntityReference])
 [psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvlabel', [Microsoft.Xrm.Sdk.Label])
+[psobject].Assembly.GetType("System.Management.Automation.TypeAccelerators")::Add('dvchoiceoption', [Microsoft.Xrm.Sdk.Metadata.OptionMetadata])
 
-
-
-
-Export-ModuleMember -Function '*' -Variable '*'
+Export-ModuleMember -Function '*' -Variable 'DataverseSession','DataverseClient'
