c230e706f8216c88ad7cf9a7bcbedc7010ed860d
Arjan Meskers <arjanmeskers@outlook.com>
2024-12-09 22:49:33 +0100

    Tests for Cmdlet and Functions Help

--- tests/AMSoftware.Dataverse.PowerShell.Tests.ps1
+++ tests/AMSoftware.Dataverse.PowerShell.Tests.ps1
@@ -1,10 +1,19 @@
+BeforeDiscovery {
+	$TestDirectory = Split-Path $PSCommandPath -Parent
+	$RootDirectory = Split-Path $TestDirectory -Parent
+	$ModuleName = (Split-Path $PSCommandPath -Leaf) -Replace ".Tests.ps1"
+
+	$ModuleDirectory = Resolve-Path -Path (Join-Path -Path $RootDirectory -ChildPath ".\dist\$($ModuleName)")
+	$ManifestPath   =  Join-Path -Path $ModuleDirectory -ChildPath "$ModuleName.psd1"
+}
+
 BeforeAll {
 	$TestDirectory = Split-Path $PSCommandPath -Parent
 	$RootDirectory = Split-Path $TestDirectory -Parent
 	$ModuleName = (Split-Path $PSCommandPath -Leaf) -Replace ".Tests.ps1"
 
 	$ModuleDirectory = Resolve-Path -Path (Join-Path -Path $RootDirectory -ChildPath ".\dist\$($ModuleName)")
-	$ManifestPath   = "$ModuleDirectory\$ModuleName.psd1"
+	$ManifestPath   =  Join-Path -Path $ModuleDirectory -ChildPath "$ModuleName.psd1"
 }
 
 Describe "AMSoftware.Dataverse.PowerShell" {
@@ -35,6 +44,43 @@ Describe "AMSoftware.Dataverse.PowerShell" {
 			Invoke-ScriptAnalyzer -Path $ModuleDirectory -IncludeRule $RuleName |
 			Foreach-Object { "Problem in $($_.ScriptName) at line $($_.Line) with message: $($_.Message)" } |
 			 Should -BeNullOrEmpty
+		}
+	}
+
+	Context "External Help" {
+		BeforeDiscovery {
+			$m = Import-PowershellDataFile $ManifestPath
+
+			$commands = $m.CmdletsToExport
+			$functions = $m.FunctionsToExport
+		}
+
+		BeforeAll {
+			Import-Module -Name $ManifestPath -Scope Local -Force
+		}
+
+		It 'Cmdlet <_> has external Help' -ForEach $commands {
+			Get-Command -Name $_ | Select-Object -ExpandProperty 'HelpFile' | Should -BeLike 'AMSoftware.Dataverse.PowerShell.*-Help.xml'
+		}
+
+		It 'Cmdlet <_> has online Help' -ForEach $commands {
+			$h = Get-Help -Name $_
+			$link = $h.relatedLinks.navigationLink[0].uri
+
+			$link | Should -Not -BeNullOrEmpty
+			$link | Should -Be "https://github.com/AMSoftwareNL/DataversePowershell/blob/main/docs/$_.md"
+		}
+
+		It 'Function <_> has external Help' -ForEach $functions {
+			Get-Command -Name $_ | Select-Object -ExpandProperty 'HelpFile' | Should -BeLike 'AMSoftware.Dataverse.PowerShell.*-Help.xml'
+		}
+
+		It 'Function <_> has online Help' -ForEach $functions {
+			$h = Get-Help -Name $_
+			$link = $h.relatedLinks.navigationLink[0].uri
+
+			$link | Should -Not -BeNullOrEmpty
+			$link | Should -Be "https://github.com/AMSoftwareNL/DataversePowershell/blob/main/docs/$_.md"
 		}
 	}
 }
